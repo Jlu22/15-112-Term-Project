61a48c515534ca8981184b97319a86b6abec55d3
Julia <julial1@andrew.cmu.edu>
2018-04-24 03:09:39 -0400

    changes to menu

--- __init__.py
+++ __init__.py
@@ -12,13 +12,20 @@ class PyCAD(PygameGame):
         self.modelList = []
     
     def mousePressed(self, x, y):
+        print(x, y)
         if self.mode == "menu":
             self.menuMousePressed(x, y)
+        elif self.mode == "create":
+            self.createMousePressed(x, y)
+        elif self.mode == "assem":
+            self.assemMousePressed(x, y)
+        elif self.mode == "help":
+            self.helpMousePressed(x, y)
     
     def mouseDrag(self, x, y):
         if self.mode == "create":
             for model in self.modelList:
-                model.mouseDrag( x, y)
+                model.mouseDrag(x, y)
     
     def mouseMotion(self, x, y):
         if self.mode == "menu":
@@ -29,26 +36,39 @@ class PyCAD(PygameGame):
             self.menuKeyPressed(keyCode, modifier)
         elif self.mode == "create":
             self.createKeyPressed(keyCode, modifier)
+        elif self.mode == "assem":
+            self.assemKeyPressed(keyCode, modifier)
+        elif self.mode == "help":
+            self.helpKeyPressed(keyCode, modifier)
     
     def timerFired(self, dt):
         if self.mode == "menu":
             self.menuTimerFired(dt)
         elif self.mode == "create":
             self.createTimerFired(dt)
+        elif self.mode == "assem":
+            self.assemTimerFired(dt)
 
     def redrawAll(self, screen):
         if self.mode == "menu":
             self.menuRedrawAll(screen)
         elif self.mode == "create":
             self.createRedrawAll(screen)
+        elif self.mode == "assem":
+            self.assemRedrawAll(screen)
+        elif self.mode == "help":
+            self.helpRedrawAll(screen)
 
 ##          Menu          ##
-    
-    def menuInit(self):
-        pass
-        
+
     def menuMousePressed(self, x, y):
-        self.mode = "create"
+        if (170 <= x <= 380) and (280 <= y <= 340):
+            self.mode = "create"
+        elif (430 <= x <= 650) and (280 <= y <= 340):
+            self.mode = "assem"
+        elif (170 <= x <= 380) and (360 <= y <= 420):
+            self.mode = "help"
+        #elif (430 <= x <= 650) and (360 <= y <= 420):
     
     def menuKeyPressed(self, keyCode, modifier):
         pass
@@ -58,15 +78,16 @@ class PyCAD(PygameGame):
     
     def menuRedrawAll(self, screen):
         self.bgColor = (217, 224, 247)
-        pygame.draw.rect(screen, (255, 0, 0), (300, 270, 200, 50))
-        pygame.draw.rect(screen, (0, 0, 255), (300, 350, 200, 50))
+        pygame.draw.rect(screen, (255, 0, 0), (170, 280, 210, 60))
+        pygame.draw.rect(screen, (0, 0, 255), (430, 280, 210, 60))
+        pygame.draw.rect(screen, (0, 255, 100), (170, 360, 210, 60))
+        pygame.draw.rect(screen, (100, 100, 0), (430, 360, 210, 60))
         pygame.display.flip()
 
 ##          Create Model          ##
 
-    def createTimerFired(self, dt):
-        for model in self.modelList:
-            model.timerFired(dt)
+    def createMousePressed(self, x, y):
+        pass
     
     def createKeyPressed(self, keyCode, modifier):
         if keyCode == pygame.K_n:
@@ -75,15 +96,47 @@ class PyCAD(PygameGame):
             self.modelList = []
             self.mode = "menu"
     
+    def createTimerFired(self, dt):
+        for model in self.modelList:
+            model.timerFired(dt)
+    
     def createRedrawAll(self, screen):
-        self.bgColor = (178, 170, 158)
+        self.bgColor = (178, 0, 158)
         for model in self.modelList:
             model.redrawAll(screen)
         pygame.display.update((0, 50, 800, 400))
 
 ##          Assembly          ##
 
+    def assemMousePressed(self, x, y):
+        pass
+    
+    def assemKeyPressed(self, keyCode, modifier):
+        if keyCode == pygame.K_n:
+            self.modelList.append(Model(self.width, self.height, self._keys))
+        if keyCode == pygame.K_m:
+            self.modelList = []
+            self.mode = "menu"
+    
+    def assemTimerFired(self, dt):
+        for model in self.modelList:
+            model.timerFired(dt)
+    
+    def assemRedrawAll(self, screen):
+        self.bgColor = (178, 170, 158)
+        for model in self.modelList:
+            model.redrawAll(screen)
+        pygame.display.update((0, 50, 800, 400))
 
 ##          Help Screen          ##
 
+    def helpMousePressed(self, x, y):
+        pass
+    
+    def helpKeyPressed(self, keyCode, modifier):
+        pass
+    
+    def helpRedrawAll(self, screen):
+        pass
+    
 PyCAD(800, 500).run()
